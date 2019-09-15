b9aaf587da961292c0a93f6cf51aa46e62554fec
n1kko777 <3734489@gmail.com>
2019-09-15 21:10:38 +0500

    AddBtn & AddLogModal

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import M from "materialize-css/dist/js/materialize.min.js";
 
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
 
 import "./App.css";
 
@@ -16,6 +18,8 @@ const App = () => {
     <>
       <SearchBar />
       <div className='container'>
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </>
---
+++ src/components/logs/AddLogModal.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import M from "materialize-css/dist/js/materialize.min.js";
+
+const AddLogModal = () => {
+  const [message, setMessage] = useState("");
+  const [attention, setAttention] = useState(false);
+  const [tech, setTech] = useState("");
+
+  const onSubmit = () => {
+    if (message === "" || tech === "") {
+      M.toast({ html: "Пожалуйста заполните все поля!" });
+    } else {
+      console.log(message, tech, attention);
+    }
+  };
+  return (
+    <div id='add-log-modal' className='modal' style={modalStyle}>
+      <div className='modal-content'>
+        <h4>Новое сообщение</h4>
+        <div className='row'>
+          <div className='input-field'>
+            <input
+              type='text'
+              name='message'
+              value={message}
+              onChange={e => setMessage(e.target.value)}
+            />
+            <label htmlFor='message' className='active'>
+              Текст сообщения
+            </label>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='input-field'>
+            <select
+              name='tech'
+              value={tech}
+              className='browser-default'
+              onChange={e => setTech(e.target.value)}
+            >
+              <option value='' disabled>
+                Выберите специалиста
+              </option>
+              <option value='Nikita Tarasov'>Nikita Tarasov</option>
+              <option value='John Doe'>John Doe</option>
+              <option value='Sam Smith'>Sam Smith</option>
+            </select>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div className='input-field'>
+            <p>
+              <label>
+                <input
+                  type='checkbox'
+                  className='filled-in'
+                  checked={attention}
+                  value={attention}
+                  onChange={e => setAttention(!attention)}
+                />
+                <span>Приоритетная</span>
+              </label>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className='modal-footer'>
+        <a
+          href='#!'
+          onClick={onSubmit}
+          className='modal-close blue waves-effect waves-green btn'
+        >
+          Добавить
+        </a>
+      </div>
+    </div>
+  );
+};
+
+const modalStyle = {
+  width: "75%",
+  height: "75%"
+};
+
+export default AddLogModal;
