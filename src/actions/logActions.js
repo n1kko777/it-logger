dd4b06c60306f9d3d82ca34020a5dc2adfe0b11b
n1kko777 <3734489@gmail.com>
2019-09-29 19:20:49 +0500

    Search component redux

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -6,7 +6,8 @@ import {
   DELETE_LOG,
   SET_CURRENT,
   CLEAR_CURRENT,
-  UPDATE_LOG
+  UPDATE_LOG,
+  SEARCH_LOGS
 } from "./types";
 
 // Get logs from server
@@ -99,6 +100,26 @@ export const updateLog = log => async dispatch => {
     dispatch({
       type: LOGS_ERROR,
       payload: err.response.statusText
+    });
+  }
+};
+
+// Search logs
+export const searchLogs = text => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs?q=${text}`);
+    const data = await res.json();
+
+    dispatch({
+      type: SEARCH_LOGS,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.statusText
     });
   }
 };
